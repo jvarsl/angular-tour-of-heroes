cf9eef7fd37929493c837f5b901b081353c2e8b2
jvarsl <j.varslauskas@gmail.com>
2021-10-10 22:48:36 +0300

    2 sections done

---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, NgModule } from '@angular/core';
+import { Hero } from 'src/app/hero';
+import { HEROES } from 'src/app/mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  heroes = HEROES;
+  selectedHero!: Hero;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+  }
+}
